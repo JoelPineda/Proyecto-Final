695fdcc6f8259aa343109319348d8b06d17c5c01
Yordin Checo <Yordincheco@gmail.com>
2021-03-05 08:42:51 -0400

    Modulo company

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -10,6 +10,14 @@ import Login from "../../views/Home_user/login";
 import Logout from "../../views/Home_user/logout";
 import MyChangePassword from "../../views/Profile/change_password";
 import { getUser, getPolicieSession, getMenu } from "../../utils/Common";
+import Employee from "../../views/Dashboard/employee";
+import EmployeeVisit from "../../views/Dashboard/employeeVisits";
+import EmployeeVisitMonth from "../../views/Dashboard/employeeVisitsMonth";
+import EmployeeVisitLastMonth from "../../views/Dashboard/employeeVisitsLastMonth";
+import AcceptePolicy from "../../views/Dashboard/policyaccepted";
+import CompanyConf from "../../views/Company/company";
+import NewCompany from "../../views/Company/AddCompany";
+import EditCompany from "../../views/Company/EditCompany";
 
 export default class Menu extends React.Component {
   constructor(props) {
@@ -17,43 +25,56 @@ export default class Menu extends React.Component {
     this.state = {
       logout: false,
     };
-
-    
-
   }
- 
-  render() {
 
+  render() {
     function changeText(event) {
       removeUserSession();
     }
 
     return (
       <Router>
-        <header id="header" className="main-header" >
-          <nav className="navbar navbar-static-top">{getToken() ? <a href="#menu">Menu</a> : <></>}</nav>
+        <header id="header" className="main-header">
+          <nav className="navbar navbar-static-top">
+            {getToken() ? <a href="#menu">Menu</a> : <></>}
+          </nav>
           <Link to="/" className="logo">
             <center>
               {getToken() ? <i className="fa fa-user-circle">&nbsp;</i> : <></>}
-              {getToken() ? getUser().user  : <></>}
-               
+              {getToken() ? getUser().user : <></>}
             </center>
           </Link>
         </header>
-      <div className="col-4 ">
-        <nav className="navbar navbar-static-top" id="menu">
-          <section class="sidebar">
-            {getToken() ? <div dangerouslySetInnerHTML={{ __html: getMenu() }} />: <></>} 
-          </section>   
-        </nav>       
-      </div>
+        <div className="col-4 ">
+          <nav className="navbar navbar-static-top" id="menu">
+            <section class="sidebar">
+              {getToken() ? (
+                <div dangerouslySetInnerHTML={{ __html: getMenu() }} />
+              ) : (
+                <></>
+              )}
+            </section>
+          </nav>
+        </div>
 
         <Switch>
           <PublicRoute path="/Login" component={Login} />
           <PrivateRoute path="/logout" component={Logout} />
           <PrivateRoute exact path="/" component={dasboard} />
           <PrivateRoute exact path="/evaluation" component={Evaluation} />
-          <PrivateRoute path="/changepassword" component={MyChangePassword} />            
+          <PrivateRoute path="/changepassword" component={MyChangePassword} />
+          <PrivateRoute path="/employeeVisit" component={Employee} />
+          <PrivateRoute path="/employeeVisitMonth" component={EmployeeVisit} />
+          <PrivateRoute
+            path="/employeeVisitLastMonth"
+            component={EmployeeVisitMonth}
+          />
+          <PrivateRoute path="/employee" component={EmployeeVisitLastMonth} />
+          <PrivateRoute path="/policyAccepted" component={AcceptePolicy} />
+
+          <PrivateRoute path="/settings" component={CompanyConf} />
+          <PrivateRoute path="/addCompany" component={NewCompany} />
+          <PrivateRoute path="/editCompany" component={EditCompany} />
         </Switch>
       </Router>
     );
---
+++ src/views/Company/company.jsx
@@ -0,0 +1,228 @@
+import React, { useState, useEffect } from "react";
+import Button from "../../components/Button/Button";
+import DatePicker, { registerLocale, setDefaultLocale } from "react-datepicker";
+import $ from "jquery";
+import Moment from "moment";
+import es from "date-fns/locale/es";
+import "moment/locale/es";
+import { getUser, removeUserSession } from "../../utils/Common";
+import API from "../../utils/api";
+import { EditEvaluation } from "../Evaluation/editEvaluation";
+
+import Loading from "../../components/loading/loading";
+import {
+  ShowConfirmationMessage,
+  MessageResults,
+  ShowPopUp,
+} from "../../utils/CommonFunctions";
+import { DataTable } from "datatables.net";
+import { LangSpanish } from "../../tools/dataTables.Language";
+
+$(document).ready(() => {
+  $("body").on("click", "#TblCompany #btEdit", function (e) {
+    ShowPopUp({
+      titleName: "Agregar Compañia",
+      htmlBody:
+        "<br /><div id='divEditEvaluation' class='container d-flex flex-column'><hr />" +
+        " <div class='row'> <div class='form-group'>" +
+        "<div class='divLeft'><label for='tbEvaluationName'>Nombre&nbsp;Compañia*:</label><span class='title-required fa faa-flash animated'></span></div>" +
+        "<input type='text' class='form-control capitalized' id='tbEvaluationName' title='Nombre de evaluación'     readonly />" +
+        "   <div class='form-group'>" +
+        "<div class='divLeft'><label for='tbEvaluationName'>Vision*:</label><span class='title-required fa faa-flash animated'></span></div>" +
+        "<input type='text' class='form-control capitalized' id='tbEvaluationName' title='Nombre de evaluación'     readonly />" +
+        "</div> " +
+        "</div></div>" +
+        "</div><hr />",
+      TextOk: "Guardar",
+      isDisabled: true,
+      EnabledDisabled: true,
+    });
+  });
+});
+
+export default function CompanyConf(props) {
+  const [dataLoading, setDataLoading] = useState(true);
+  const [company, setCompany] = useState(true);
+
+  const fillData = () => {
+    let Record = [];
+    API.getData("/Company/getCompany")
+      .then((res) => {
+        setDataLoading(false);
+        if (res.status === 200) {
+          let dataResult = [];
+          /*  let calendarBtn =
+            "<a href='#' id='btCalendar'  class='fa fa-calendar custom-color size-effect-x2' title='Calendario Evaluación' ></a>&nbsp;";
+          let PossibleAswBtn =
+            "<a href='#' id='btPossibleAsw'  class='fa fa-reply custom-color size-effect-x2' title='Posibles Respuestas' ></a>&nbsp;";
+          let AswBtn =
+            "<a href='#' id='btAsw' id='btEdit' class='fa fa-question custom-color size-effect-x2' title='Preguntas' ></a>&nbsp;";*/
+          let EditBtn =
+            "<a href='/addCompany'   class='fa fa-pencil-square-o custom-color size-effect-x2' title='Editar Evaluación' ></a>";
+          setCompany(res.data);
+          debugger;
+          res.data.forEach((item) => {
+            dataResult.push({
+              id:
+                '<span class="container d-flex align-items-center justify-content-center">' +
+                item.id +
+                "</>",
+              logo:
+                '<span class="capitalized defaultText">' +
+                item.logo +
+                "</span>",
+              vision:
+                '<span class="capitalized defaultText">' +
+                item.vision +
+                "</span>",
+              values:
+                '<span class="capitalized defaultText">' +
+                item.values +
+                "</span>",
+              name:
+                '<span class="capitalized defaultText">' +
+                item.name +
+                "</span>",
+              mision:
+                '<span class="capitalized defaultText">' +
+                item.mision +
+                "</span>",
+              itemBtn:
+                '<a class="fa fa-pencil-square-o custom-color size-effect-x2"   title="Editar Compañia" href="/editCompany?id=' +
+                item.id +
+                '"' +
+                item.id +
+                " ></a>",
+            });
+          });
+
+          $("#TblCompany").DataTable({
+            destroy: true,
+            searching: false,
+            language: LangSpanish,
+            bLengthChange: false,
+            lengthMenu: [10, 20, 40, 60, 80, 90, 100, 200],
+            order: [[0, "desc"]],
+            dom: "Bfrtip",
+            buttons: ["copy", "excel", "pdf"],
+            data:
+              dataResult.length === 0
+                ? [
+                    {
+                      logo: "",
+                      vision: "",
+                      name: "",
+                      mision: "",
+                      values: "",
+                      itemBtn: "",
+                    },
+                  ]
+                : dataResult,
+            columns: [
+              {
+                data: "name",
+                title: "Nombre ",
+                width: "15%",
+                className: "capitalized",
+              },
+              {
+                data: "logo",
+                title: "Logo\u00a0Compania",
+                width: "25%",
+                className: "capitalized",
+              },
+              {
+                data: "vision",
+                title: "Vision",
+                width: "20%",
+                className: "capitalized",
+              },
+              {
+                data: "values",
+                title: "Valores",
+                width: "20%",
+                className: "capitalized",
+              },
+
+              {
+                data: "mision",
+                title: "Mision ",
+                width: "15%",
+                className: "capitalized",
+              },
+              {
+                data: "itemBtn",
+                title: "\u00a0Acciones\u00a0\u00a0\u00a0",
+                width: "30%",
+                orderable: false,
+              },
+            ],
+          });
+          $(".csHidden").attr("style", "display:none");
+        }
+      })
+      .catch(function (err) {
+        console.error("Error de conexion " + err);
+      });
+    setDataLoading(false);
+  };
+
+  useEffect(() => {
+    fillData();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <br />
+        <div className=" htmlPayroll    container">
+          <div className="row">
+            <div className="lowcolor col-12">
+              <br />
+              <br />
+              <h2 className="h2">CompaÑia</h2>
+              <a href="/addCompany">
+                <span className="btn btn-success btn-sm">
+                  <i className="fa fa-plus-circle"></i>&nbsp;Añadir Compania
+                </span>
+              </a>
+            </div>
+          </div>
+          <div className="row ">
+            {!dataLoading ? (
+              <>
+                <div className="container">
+                  <div className="">
+                    {!dataLoading ? (
+                      <div
+                        className="scroll-table bordered"
+                        Style="min-height:600px"
+                      >
+                        <table
+                          id="TblCompany"
+                          className="table table-striped table-bordered display"
+                          Style="width:100% !important"
+                        ></table>
+                      </div>
+                    ) : (
+                      <Loading />
+                    )}
+                  </div>
+                </div>
+              </>
+            ) : (
+              <div className="container">
+                <Loading />
+              </div>
+            )}
+          </div>
+
+          <br />
+          <br />
+        </div>
+        <br />
+        <br />
+      </div>
+    </>
+  );
+}
